ede4401c1a807b07128ef2cbb9b3000b2e6db00a
Иван <ivankiseliov@rambler.ru>
2022-11-04 19:37:40 +0300

    3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap <String, String> phoneBook = new HashMap<>();
+        phoneBook.put("Ivan Ivanov", "+7999-999-99-99");
+        phoneBook.put("Ivan Petrov", "+7939-959-99-99");
+        phoneBook.put("Sergei Ivanov", "+7699-994-99-99");
+        phoneBook.put("Vitaliy Ivanov", "+7399-999-99-49");
+        phoneBook.put("Evgeniy Ivanov", "+7999-999-99-99");
+        phoneBook.put("Ivan Sidorov", "+7999-949-99-99");
+        phoneBook.put("Aram Petrov", "+7979-999-99-99");
+        phoneBook.put("Ivan Semenov", "+7999-999-99-99");
+        phoneBook.put("Ivan Viktorov", "+7299-999-99-99");
+        phoneBook.put("Viktor Ivanov", "+7959-999-99-99");
+        phoneBook.put("Nikolay Ivanov", "+7989-999-99-99");
+        phoneBook.put("Ivan Nikolaev", "+7999-999-99-99");
+        phoneBook.put("Oleg Ivanov", "+7299-999-99-99");
+        phoneBook.put("Kirill Ivanov", "+7999-999-99-99");
+        phoneBook.put("Ivan Olegov", "+7999-939-99-99");
+        phoneBook.put("Oleg Petrov", "+7999-994-99-99");
+        phoneBook.put("Kirill Semenov", "+7999-959-99-99");
+        phoneBook.put("Vitaliy Olegov", "+7999-969-99-99");
+        phoneBook.put("Petr Ivanov", "+7999-799-99-99");
+        phoneBook.put("Konstantin Ivanov", "+7995-999-99-99");
+        System.out.println(phoneBook);
     }
 }
